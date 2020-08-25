03fa4e990b542c3db769c44b420762233f251ad1
John Rykken <john.rykken@gmail.com>
2020-08-24 19:07:48 -0700

    begin styling quiz cards

--- src/components/Swipe.tsx
+++ src/components/Swipe.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { View, Text } from 'react-native'
 import { 
   GLOBAL_STYLES as STYLES,
@@ -16,20 +16,27 @@ interface Props {
 
 const Swipe: React.FC<Props> = ({ questions, onSwipe }) => {
   
-  console.log('questions in swipe component!', questions) // REMOVE
-  const swiper = useRef(null) // PERHAPS UNNECESSARY 
   const entities = new Entities()
-  const cards = questions.map((q: ObjectType) => <Card style={styles.card}><Text style={STYLES.largeText}>{entities.decode(q.question)}</Text></Card>)
+  const cards = questions.map((q: ObjectType) => (
+    <Card style={styles.card} key={Math.random().toString()}>
+      <View style={STYLES.standard}>
+        <Text style={STYLES.subHeaderText}>
+          {entities.decode(q.category)}
+        </Text>
+      </View>
+      <View style={STYLES.standard}>
+        <Text style={STYLES.regularText}>
+          {entities.decode(q.question)}
+        </Text>
+      </View>
+    </Card>)
+  ) 
 
   return (
     <View style={STYLES.container}>
-      <View style={STYLES.standard}>
-        <Text style={STYLES.headerText}>{questions[0].category}</Text>
-      </View>
       <CardStack
         style={styles.content}
         renderNoMoreCards={() => <Text style={{ fontWeight: '700', fontSize: 18, color: 'gray' }}>done</Text>}
-        ref={swiper} // PERHAPS UNNECESSARY
         onSwipedLeft={(index: number) => onSwipe(index, false)}
         onSwipedRight={(index: number) => onSwipe(index, true)}
       >
@@ -41,6 +48,7 @@ const Swipe: React.FC<Props> = ({ questions, onSwipe }) => {
      
     </View>
   )
+
 }
 
 export default Swipe
