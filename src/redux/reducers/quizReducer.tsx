e31e502463989cb8903772ed203eb901b2ae789e
John Rykken <john.rykken@gmail.com>
2020-08-26 11:31:13 -0700

    refactor quiz reducer so that it handles updating and resetting the scoreboard

--- src/redux/reducers/quizReducer.tsx
+++ src/redux/reducers/quizReducer.tsx
@@ -4,9 +4,9 @@ import {
   UPDATE_SCOREBOARD,
   RESET_SCOREBOARD 
 } from '../actions/types'
-import { ActionType } from '../../reusableTypes'
+import { ActionType, ObjectType } from '../../reusableTypes'
 
-const initialState = { score: 0 }
+const initialState: {score: number, scoreBoard: ObjectType[]} = { score: 0, scoreBoard: [] }
 
 // ADD TYPESCRIPT RETURN TYPE? 
 const quizReducer = (state = initialState, action: ActionType) => {
@@ -15,6 +15,15 @@ const quizReducer = (state = initialState, action: ActionType) => {
       return { ...state, score: action.payload }
     case RESET_QUIZ_SCORE: 
       return { ...state, score: 0 }
+    case UPDATE_SCOREBOARD:
+      const { scoreBoard } = state
+      scoreBoard[action.payload.index] = { 
+        answeredCorrectly: action.payload.answeredCorrectly 
+      } 
+      console.log('updated scoreboard', scoreBoard) 
+      return { ...state, scoreBoard }
+    case RESET_SCOREBOARD: 
+      return { ...state, scoreBoard: [] }
     default: 
       return state 
   }
