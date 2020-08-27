62a3edffee08bbbe9aad343f57a2d3a6cb3336de
John Rykken <john.rykken@gmail.com>
2020-08-27 14:11:42 -0700

    add basic header and footer to FlatListComponent

--- src/screens/DoneScreen.tsx
+++ src/screens/DoneScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, View, FlatList, Text } from 'react-native'
+import { SafeAreaView, TouchableOpacity, FlatList, Text } from 'react-native'
 import { GLOBAL_STYLES as STYLES } from '../styles'
 import { ObjectType } from '../reusableTypes'
 import { connect } from 'react-redux'
@@ -13,30 +13,36 @@ interface Props {
 
 const DoneScreen: React.FC<Props> = ({ navigation, questions, score, totalQuestionsAnswered }) => {
 
-  console.log('done screen questions', questions) // remove
 
+  const header = <><Text style={STYLES.headerText}>{((score / questions.length) * 100).toString() + '%'}</Text></>
+  const footer = ( // add actual text! 
+    <TouchableOpacity onPress={() => navigation.popToTop()}>
+      <Text>PLAY AGAIN</Text> 
+    </TouchableOpacity>
+  )
+  
   const renderItem = (item: ObjectType) => {
     console.log('item.correct_answer', item.correct_answer)
     const userAnswer = item.user_answered_correctly ? item.correct_answer : !item.correct_answer
     return (
-      <View>
-        <Text>question: {item.question}</Text>
+      <>
+        <Text>{item.question}</Text>
         <Text>correct answer: {item.correct_answer.toString()}</Text>
         <Text>your answer: {userAnswer.toString()}</Text>
-      </View>
+      </>
     )
-  } 
-
+  }
+  
   return (
     <SafeAreaView style={STYLES.standard}>
-      <View> 
         <FlatList
           data={questions}
           renderItem={({ item }) => renderItem(item)} 
           keyExtractor={(item: ObjectType) => item.question}
-        />
-      </View>
+          ListHeaderComponent={header}
+          ListFooterComponent={footer}
 
+        />
     </SafeAreaView>
   )
 }
