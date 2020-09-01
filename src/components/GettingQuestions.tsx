424cc5ba40e6802009a742f8d426b0355e9540f5
John Rykken <john.rykken@gmail.com>
2020-09-01 11:54:09 -0700

    revise GettingQuestions component

--- src/components/GettingQuestions.tsx
+++ src/components/GettingQuestions.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import { SafeAreaView, View, Text } from 'react-native'
 import { GLOBAL_STYLES as STYLES } from '../styles'
+import { gettingQuestionsComponentCopy as COPY } from '../copy'
 
-// FIX THIS COMPONENT! 
-// HERE! DO THIS AND BE DONE WITH IT ... 
+
 // MAKE THE TEXT TINY AND COLORFUL AND PERHAPS EVEN FUNNY ... 
 
-const GettingQuestions = () => {
-  return (
-    <SafeAreaView>
-      <View>
-        <Text>
-          GETTING QUESTIONS
-        </Text>
-      </View>
-    </SafeAreaView>
-  )
-}
+const GettingQuestions: React.FC = () => (
+  <SafeAreaView style={STYLES.standard}>
+    <View >
+      <Text style={[STYLES.regularText, STYLES.red]}>{COPY.gettingQs}</Text>
+    </View>
+  </SafeAreaView>
+)
+
 
 export default GettingQuestions
