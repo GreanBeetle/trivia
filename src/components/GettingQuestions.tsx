882d8a9a289484f9653d993be8afb8c8b2075cd3
John Rykken <john.rykken@gmail.com>
2020-09-01 11:27:35 -0700

    Add file GettingQuestions and create preliminary 'GettingQuestions' component

---
+++ src/components/GettingQuestions.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SafeAreaView, View, Text } from 'react-native'
+import { GLOBAL_STYLES as STYLES } from '../styles'
+
+const GettingQuestions = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>
+          GETTING QUESTIONS
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default GettingQuestions
